1cceeb13adced2b918091d32a1ff952e041409ba
Tom Etzel <tom.etzel@quantum.com>
2017-07-31 15:55:24 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'hammerjs';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule, MdSidenavModule, MdToolbarModule, MdIconModule, MdButtonModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
